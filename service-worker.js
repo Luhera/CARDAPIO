769711140c224d73f5e56cbbd7dd1ed9adfd34c1
Anderson Lucena <alucena933@gmail.com>
2023-09-04 15:18:17 -0300

    pwa do inferno que nao pega

--- service-worker.js
+++ service-worker.js
@@ -38,78 +38,20 @@ self.addEventListener('install', event => {
       ]))
   );
 });
-// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
 self.addEventListener('message', function (event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('fetch', function (event) {
-  //Atualizacao internet
-  event.respondWith(async function () {
-    try {
-      return await fetch(event.request);
-    } catch (err) {
-      return caches.match(event.request);
+    if (event.data.action === 'skipWaiting') {
+      self.skipWaiting();
     }
-  }());
-
-  //Atualizacao cache
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-
-});
+  });
+  
+  self.addEventListener('fetch', function (event) {
+    //Atualizacao internet
+    event.respondWith(async function () {
+      try {
+        return await fetch(event.request);
+      } catch (err) {
+        return caches.match(event.request);
+      }
+    }());
+  });
+  
